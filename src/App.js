8542598650314ce64d0791f4cc4204fe957f7f7a
Alexandre Junior <jasj_jr@hotmail.com>
2020-05-14 09:35:26 -0300

    Remove repositories

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./styles.css";
 
 import api from './services/api';
 
 function App() {
-  const [repositories, setRepositories] = useState([
-    {
-      "url": "https://github.com/Rocketseat/umbriel",
-      "title": "Umbriel",
-      "techs": [
-        "Node",
-        "Express",
-        "TypeScript"
-      ],
-      "id": "4954f6f9-1aab-4d61-9b9b-d9cd0c34a86c",
-      "likes": 6
-    }
-  ])
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get("/repositories").then(function (response) {
+      setRepositories(response.data);
+    });
+  }, []);
 
   async function handleAddRepository() {
     const response = await api.post('repositories', {
@@ -33,7 +27,11 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`)
+
+    console.log(response.status)
+
+    setRepositories(repositories.filter(repo => repo.id !== id))
   }
 
   return (
